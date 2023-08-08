35575cca28edae3d2962f158428ac509dd675c2f
Ruobing1997 <ruobing2@andrew.cmu.edu>
2023-08-08 13:49:28 +0800

    backup

--- scripts/shell/docker_run.sh
+++ scripts/shell/docker_run.sh
@@ -3,3 +3,8 @@
 # docker build --no-cache -t morefun-supernova-manager:4.1 -f cmd/task-manager/Dockerfile .
 #run
 #docker run -d -p 8080:8080 morefun-supernova-manager:0.1
+
+
+docker build --no-cache -t morefun-supernova-manager:4.1 -f cmd/task-manager/Dockerfile .
+docker build --no-cache -t morefun-supernova-worker:4.1 -f cmd/task-executor/Dockerfile .
+docker build --no-cache -t morefun-supernova-api:4.1 -f cmd/api/Dockerfile .
